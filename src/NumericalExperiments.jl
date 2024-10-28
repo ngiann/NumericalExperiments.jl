961850f549369bdd51f20f43fb01e27ff5889e1f
Nikos Gianniotis <ngiann@gmail.com>
2024-10-28 09:19:32 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.52

---
+++ src/NumericalExperiments.jl
@@ -0,0 +1,5 @@
+module NumericalExperiments
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using NumericalExperiments
+using Test
+
+@testset "NumericalExperiments.jl" begin
+    # Write your tests here.
+end
