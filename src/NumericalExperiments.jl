b450ec58c2b90a2162a1b46e5641de67de66ef5b
Nikos Gianniotis <ngiann@gmail.com>
2024-11-02 23:18:39 +0100

    add GPCCData

--- src/NumericalExperiments.jl
+++ src/NumericalExperiments.jl
@@ -1,9 +1,15 @@
 module NumericalExperiments
 
-    using GPCC, LinearAlgebra, Random, ELBOfy, ELBOfyUtilities
+    using GPCC, GPCCData, LinearAlgebra, Random, ELBOfy, ELBOfyUtilities
 
     include("gpccloglikelihood.jl")
 
     export gpccloglikelihood
+
+    include("runapproximation.jl")
+
+    include("3C120.jl")
+
+    export run_3C120
 
 end
