79c347d89ad8daad7e25125b624959d0f8f8517e
Nikos Gianniotis <ngiann@gmail.com>
2024-11-26 12:09:27 +0100

    add Optim

--- src/NumericalExperiments.jl
+++ src/NumericalExperiments.jl
@@ -1,6 +1,6 @@
 module NumericalExperiments
 
-    using JLD2, GPCC, GPCCData, LinearAlgebra, Random, ELBOfy, ELBOfyUtilities, Distributions, ProgressMeter, ThreadTools
+    using JLD2, GPCC, GPCCData, LinearAlgebra, Random, ELBOfy, ELBOfyUtilities, Distributions, ProgressMeter, ThreadTools, Optim
 
     # Following lines makes ProgressMeter work with tmap1
 
