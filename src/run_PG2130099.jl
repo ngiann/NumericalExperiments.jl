6a7ee74bee4382d6ea48363f40cac1d62b9dcf90
Nikos Gianniotis <ngiann@gmail.com>
2024-11-26 12:09:56 +0100

    specify NelderMead optimiser

--- src/run_PG2130099.jl
+++ src/run_PG2130099.jl
@@ -22,7 +22,7 @@ function run_PG2130099(; iterations = 1)
 
         elbodiag = elbofy_diag(logp, 6, 100)
 
-        resdiag = maximise_elbo(elbodiag, randn(rng, numparam(elbodiag)), iterations = iterations)
+        resdiag = maximise_elbo(elbodiag, randn(rng, numparam(elbodiag)), iterations = iterations, Method = NelderMead())
 
         JLD2.save("PG2130099_diag.jld2", "resdiag", resdiag, "elbodiag", elbodiag)
 
@@ -35,12 +35,12 @@ function run_PG2130099(; iterations = 1)
 
         elbosphere = elbofy_sphere(logp, 6, 100)
 
-        ressphere = maximise_elbo(elbosphere, randn(rng, numparam(elbosphere)), iterations = iterations)
+        ressphere = maximise_elbo(elbosphere, randn(rng, numparam(elbosphere)), iterations = iterations, Method = NelderMead())
 
 
         elbofull = elbofy_full(logp, 6, 100)
 
-        resfull = maximise_elbo(elbofull, [ressphere.minimizer[1:6]; ressphere.minimizer[7]*vec(1.0*Matrix(I,6,6))], iterations = iterations)
+        resfull = maximise_elbo(elbofull, [ressphere.minimizer[1:6]; ressphere.minimizer[7]*vec(1.0*Matrix(I,6,6))], iterations = iterations, Method = NelderMead())
 
         JLD2.save("PG2130099_full.jld2", "resfull", resfull, "elbofull", elbofull)
         
@@ -52,11 +52,11 @@ function run_PG2130099(; iterations = 1)
 
         elbosphere = elbofy_sphere(logp, 6, 100)
 
-        ressphere = maximise_elbo(elbosphere, randn(rng, numparam(elbosphere)), iterations = iterations)
+        ressphere = maximise_elbo(elbosphere, randn(rng, numparam(elbosphere)), iterations = iterations, Method = NelderMead())
 
         elbomvi = elbofy_mvi(logp, geteigenvectors(logp, ressphere.minimizer[1:6]), 100)
 
-        resmvi = maximise_elbo(elbomvi, [ressphere.minimizer[1:6]; ones(6)*0.1], iterations = iterations)
+        resmvi = maximise_elbo(elbomvi, [ressphere.minimizer[1:6]; ones(6)*0.1], iterations = iterations, Method = NelderMead())
 
         JLD2.save("PG2130099_mvi.jld2", "resmvi", resmvi, "elbomvi", elbomvi)
 
@@ -69,13 +69,13 @@ function run_PG2130099(; iterations = 1)
 
         elbomviext = elbofy_mvi_ext(logp, 1.0*Matrix(I,6,6), 100)
 
-        resmviext = maximise_elbo(elbomviext, randn(rng, numparam(elbomviext)), iterations = iterations)
+        resmviext = maximise_elbo(elbomviext, randn(rng, numparam(elbomviext)), iterations = iterations, Method = NelderMead())
 
         for _ in 1:10
 
             elbomviext, resmviext = updatecovariance(elbomviext, resmviext)
 
-            resmviext = maximise_elbo(elbomviext, getsolution(resmviext), iterations = iterations)
+            resmviext = maximise_elbo(elbomviext, getsolution(resmviext), iterations = iterations, Method = NelderMead())
 
         end
 
@@ -90,13 +90,13 @@ function run_PG2130099(; iterations = 1)
 
         elboskewext = elbofy_skewdiag_ext(logp, 1.0*Matrix(I,6,6), 100)
 
-        resskewext = maximise_elbo(elboskewext, randn(rng, numparam(elboskewext)), iterations = iterations)
+        resskewext = maximise_elbo(elboskewext, randn(rng, numparam(elboskewext)), iterations = iterations, Method = NelderMead())
 
         for _ in 1:10
 
             elboskewext, resskewext = updatecovariance(elboskewext, resskewext)
 
-            resskewext = maximise_elbo(elboskewext, getsolution(resskewext), iterations = iterations)
+            resskewext = maximise_elbo(elboskewext, getsolution(resskewext), iterations = iterations, Method = NelderMead())
 
         end
 
