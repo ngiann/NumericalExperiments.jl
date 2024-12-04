3a4cdc1f05cbf3237cdd22c5f6fa5e08eb3d3ec2
Nikos Gianniotis <ngiann@gmail.com>
2024-12-04 09:56:16 +0100

    put print statements

--- src/PG213009/warmup_PG.jl
+++ src/PG213009/warmup_PG.jl
@@ -5,6 +5,8 @@ function warmup_PG()
 
     let
 
+        @printf("\n=> Warm up for spherical and mixture thereof\n")
+
         elbosphere = elbofy_sphere(logp, 6, 30)
 
         maximise_elbo(elbosphere, iterations = 3, Method = NelderMead())
@@ -18,6 +20,8 @@ function warmup_PG()
 
     let
 
+        @printf("\n=> Warm up for diagonal and mixture thereof\n")
+
         elbodiag = elbofy_diag(logp, 6, 30)
 
         maximise_elbo(elbodiag, iterations = 3, Method = NelderMead())
@@ -30,6 +34,8 @@ function warmup_PG()
 
     let
 
+        @printf("\n=> Warm up for fullcov and mixture thereof\n")
+
         elbofull = elbofy_full(logp, 6, 30)
 
         maximise_elbo(elbofull, iterations = 3, Method = NelderMead())
@@ -41,6 +47,8 @@ function warmup_PG()
     end
 
     let
+
+        @printf("\n=> Warm up for mvi ext and mixture thereof\n")
         
         elbomviext = elbofy_mvi_ext(logp, 1.0*Matrix(I,6,6), 30)
 
@@ -50,7 +58,8 @@ function warmup_PG()
 
         maximise_elbo(elbomviext, getsolution(resmviext), iterations = 3, Method = NelderMead())
 
-        elbomix = elbofy_mixture(ELBOfy.ElboMviEXT, logp, 6, 30, K = 3)
+
+        elbomix = elbofy_mixture(ELBOfy.ElboMviExt, logp, 6, 30, K = 3)
 
         maximise_elbo(elbomix, iterations = 3, Method = NelderMead())
         
